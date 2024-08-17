0e045db4c9e9a209184f58b51358e0923e754b0f
JoaoFernandoGoncalves <ra120109@uem.br>
2024-08-17 17:19:17 -0300

    Att

--- DeadLineTSP.py
+++ DeadLineTSP.py
@@ -8,7 +8,7 @@ def leituraInstancia(nomeInstancia):
     arqv = open(nomeInstancia, "r")
 
     numVertices = int(arqv.readline())
-    matrizCustos = np.empty((21,21))
+    matrizCustos = np.empty((21,21), dtype=int)
     
     for i in range (numVertices):
         linha = list(map(int, arqv.readline().split()))
@@ -22,21 +22,21 @@ def leituraInstancia(nomeInstancia):
 
     return numVertices, matrizCustos, arestas, prazos
 
-#TESTE
-
 numVertices, custos, arestas, prazos = leituraInstancia(nomeInstancias[0])
+
+#print(f"\n\nResolução do Caixeiro Viajante com Prazos para a instancia: {nome}\n\n")
 
 #Inicializando o LP
 dl_tsp = LpProblem("Caxeiro_Viajante_com_Prazos", LpMinimize)
 
 #Variaveis de decisao
 x = LpVariable.dicts("x", arestas, cat = "Binary")
-t = LpVariable.dicts("t", [i for i in range(numVertices)], lowBound = 1, upBound = numVertices, cat = "Continuous")
+t = LpVariable.dicts("t", [i for i in range(numVertices)], lowBound = 0, upBound = None, cat = "Integer")
 
 #Funcao objetivo
 dl_tsp += lpSum([custos[i][j] * x[i, j] for (i, j) in arestas])
 
-#Restricoes de fluxo de saida dos vertices
+#Restricoes de fluxo de entrada e saida dos vertices
 for j in range(numVertices):
     dl_tsp += lpSum([x[i, j] for (i, u) in arestas if u == j]) == 1
 
@@ -44,14 +44,17 @@ for i in range(numVertices):
     dl_tsp += lpSum([x[i, j] for (u, j) in arestas if u == i]) == 1
 
 #Restricao de eliminação de subrotas
-M = 100000
+M = [[max(prazos[i] + custos[i][j], 0) for j in range(numVertices)] for i in range(numVertices)]
+#M = 100000
+
 for (i, j) in arestas:
     if i > 0:
-        dl_tsp += t[i] + custos[i][j] - t[j] <= M * (1 - x[i , j])
+        dl_tsp += t[i] + custos[i][j] - t[j] <= M[i][j] * (1 - x[i, j])
 
 #Restricao de prazo
 for i in range(numVertices):
-    dl_tsp += t[i] <= prazos[i]
+    if prazos[i] > 0:
+        dl_tsp += t[i] <= prazos[i]
 
 #Resolvendo
 resolucao = dl_tsp.solve()
